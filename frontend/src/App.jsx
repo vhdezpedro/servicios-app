15b63884f0ae460a8c7eafc5e34aa5f3b482703f
Pedro Villanueva <vhdezpedro@gmail.com>
2026-06-27 22:10:45 +0300

    Se crea el componente Hero.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Login />
     </>
   );
