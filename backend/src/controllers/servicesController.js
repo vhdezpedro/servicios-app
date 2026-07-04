a0f4314a4b4f0bbd417adca8cdb56b3ebb039fa5
Pedro Villanueva <vhdezpedro@gmail.com>
2026-07-04 07:32:01 +0300

    Se crean los controladores y las rutas para crear, actualizar y borrar registros de los servicios.

--- backend/src/controllers/servicesController.js
+++ backend/src/controllers/servicesController.js
@@ -16,18 +16,16 @@ export const getServices = async (req, res) => {
 export const getServicesbyId = async (req, res) => {
   const { id } = req.params;
   try {
-    const [rows] = await db.query("SELECT * FROM servicios WHERE id = ?", id);
+    const [result] = await db.query("SELECT * FROM servicios WHERE id = ?", id);
 
-    console.log("rows: ", rows);
-
-    if (!rows[0]) {
+    if (!result[0]) {
       return res
         .status(404)
         .json({ ok: false, message: "Servicio no encontrado." });
     }
 
-    res.json({ ok: true, data: rows });
-    console.log(rows);
+    res.json({ ok: true, data: result });
+    console.log(result);
   } catch (error) {
     console.error("Error al obtener el servicios: ", error.message);
     res
@@ -38,6 +36,7 @@ export const getServicesbyId = async (req, res) => {
 
 export const createService = async (req, res) => {
   const { nombre, descripcion, precio } = req.body;
+
   try {
     const [result] = await db.query(
       "INSERT INTO servicios (nombre, descripcion, precio) VALUES (?, ?, ?)",
@@ -52,5 +51,55 @@ export const createService = async (req, res) => {
   } catch (error) {
     console.error("Error al crear el servicio: ", error.message);
     res.status(500).json({ ok: false, message: "Error al crear el servicio." });
+  }
+};
+
+export const updateService = async (req, res) => {
+  const { id } = req.params;
+  const { nombre, descripcion, precio } = req.body;
+
+  try {
+    const [result] = await db.query(
+      "UPDATE servicios SET nombre = ?, descripcion = ?, precio = ? WHERE id = ?",
+      [nombre, descripcion, precio, id],
+    );
+
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "Servicio no encontrado." });
+    }
+
+    res.json({
+      ok: true,
+      message: "Servicio actualizado exitosamente.",
+      data: { id, nombre, descripcion, precio },
+    });
+  } catch (error) {
+    console.error("Error al actualizar el servicio: ", error.message);
+    res
+      .status(500)
+      .json({ ok: false, message: "Error al actualizar el servicio." });
+  }
+};
+
+export const deleteService = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [result] = await db.query("DELETE FROM servicios WHERE id = ?", id);
+
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "Servicio no encontrado." });
+    }
+
+    res.json({ ok: true, message: "Servicio eliminado exitosamente." });
+  } catch (error) {
+    console.error("Error al eliminar el servicio: ", error.message);
+    res
+      .status(500)
+      .json({ ok: false, message: "Error al eliminar el servicio." });
   }
 };
--- backend/src/routes/servicesRoutes.js
+++ backend/src/routes/servicesRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
 import {
+  createService,
+  deleteService,
   getServices,
   getServicesbyId,
+  updateService,
 } from "../controllers/servicesController.js";
 
 const router = express.Router();
 
 router.get("/api/servicios", getServices);
 router.get("/api/servicios/:id", getServicesbyId);
+router.post("/api/servicios/", createService);
+router.put("/api/servicios/:id", updateService);
+router.delete("/api/servicios/:id", deleteService);
 
 export default router;
